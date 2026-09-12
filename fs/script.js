1bbcad8ac3ac99d9094ddd0aa0a340fcafa591bc
jambasequessa33 <jambasequessa@gmail.com>
2026-09-12 12:03:58 +0100

    Delete fs directory

--- fs/script.js
+++
@@ -1,8 +0,0 @@
-function mudaFoto(foto) {
-    alert("JavaScript funcionando!");
-    document.getElementById("icone").src = foto;
-}
-abrirLightbox(0)
-fotoAnterior()
-proximaFoto()
-fecharLightbox()
