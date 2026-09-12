a4ee06750a310af7eb1b96ca4850b9c0f0b860d5
jambasequessa33 <jambasequessa@gmail.com>
2026-09-12 11:26:39 +0100

    Add files via upload

---
+++ fs/script.js
@@ -0,0 +1,8 @@
+function mudaFoto(foto) {
+    alert("JavaScript funcionando!");
+    document.getElementById("icone").src = foto;
+}
+abrirLightbox(0)
+fotoAnterior()
+proximaFoto()
+fecharLightbox()
